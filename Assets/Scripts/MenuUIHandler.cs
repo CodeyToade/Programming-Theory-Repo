5585a6d2d9c553066fc4bf646ea2805ac466f4ba
CodeyToade <leo.lane@gmx.com>
2023-12-01 01:09:59 -0500

    Menu Functionality + Imports
    - Imported vfx, audio fx, particles, animations, food prefabs, and ground textures
    - Added game exiting and game starting buttons

---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MenuUIHandler : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI PlayerNameInput;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void SetPlayerName()
+    {
+        MainManager.Instance.PlayerName = PlayerNameInput.text;
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+}
