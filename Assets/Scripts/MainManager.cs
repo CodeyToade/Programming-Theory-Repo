2d38ed7107d69752a589bdae2d0db612c9632424
CodeyToade <leo.lane@gmx.com>
2023-12-01 02:09:21 -0500

    Game Over
    -game over screen implemented
    -ranking text is included with game over menu
    -return to menu button with functionality added

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -8,10 +8,24 @@ using System.IO;
 
 public class MainManager : MonoBehaviour
 {
+    private bool isGameActive;
     public TextMeshProUGUI CurrentPlayerName;
+    public GameObject GameOverMenu;
 
     void Start()
     {
+        isGameActive = true;
         CurrentPlayerName.text = PlayerDataHandle.Instance.PlayerName;
+    }
+
+    public void GameOver()
+    {
+        isGameActive = false;
+        GameOverMenu.gameObject.SetActive(true);
+    }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene(0);
     }
 }
