f627a02745f0ecb1427dfb36bc8d7ffeed687014
CodeyToade <leo.lane@gmx.com>
2023-12-01 02:31:39 -0500

    lifetime and life counter
    lifetime and life counter now properly initialize with their starting game text

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -10,12 +10,19 @@ public class MainManager : MonoBehaviour
 {
     private bool isGameActive;
     public TextMeshProUGUI CurrentPlayerName;
+    public TextMeshProUGUI LifeText;
+    public TextMeshProUGUI LifetimeText;
+    private float Lives;
+    private float Time;
     public GameObject GameOverMenu;
 
     void Start()
     {
         isGameActive = true;
+        Lives = 3;
         CurrentPlayerName.text = PlayerDataHandle.Instance.PlayerName;
+        LifeText.text = "Life: " + Lives;
+        LifetimeText.text = "Lifetime: " + Time;
     }
 
     public void GameOver()
